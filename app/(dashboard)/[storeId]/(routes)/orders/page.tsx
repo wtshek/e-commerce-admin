28e1dbaa3592e5018d6915fa60131690f94307f2
wtshek <wingtung.shek@accedo.tv>
2023-08-18 10:39:12 -0400

    add stripe payment

--- app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -37,7 +37,7 @@ export const OrdersPage: FC<OrdersPageProps> = async ({ params }) => {
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={formattedOrder} />
+        <OrderClient data={formattedOrder} />
       </div>
     </div>
   );
